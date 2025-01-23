3b4e7309fbcdb2b0db7e6c2bcab6ce979b62f46f
Rierickson <riericksontorres@gmail.com>
2025-01-23 19:31:45 -0300

    add verification user

--- src/app/api/getWeek/route.ts
+++ src/app/api/getWeek/route.ts
@@ -1,22 +1,42 @@
-import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-export async function GET() {
+export async function GET(req: Request): Promise<NextResponse> {
   try {
     const prisma = new PrismaClient();
 
-    // Consulta ao banco para obter todos os registros históricos
+    // Extrai os parâmetros da query string
+    const { searchParams } = new URL(req.url);
+    const email = searchParams.get("email"); // Obtém o e-mail da query string
+
+    if (!email) {
+      return NextResponse.json(
+        { message: "Email não fornecido." },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: email }, // Replace "user-email" with the actual user email
+    });
+
+    const id = user?.id;
+
+    // Consulta ao banco para registros com base no e-mail
     const records = await prisma.cheklist.findMany({
+      where: {
+        id, // Filtra pelo e-mail fornecido
+      },
       orderBy: {
-        date: "asc",
+        date: "desc",
       },
     });
 
     return NextResponse.json(records);
   } catch (error) {
-    console.error(`Erro ao buscar dados: ${error} `);
+    console.error(`Erro ao buscar dados: ${error}`);
     return NextResponse.json(
-      { message: "Erro ao buscar os dados", error: error },
+      { message: "Erro ao buscar os dados", error: (error as Error).message },
       { status: 500 }
     );
   }
